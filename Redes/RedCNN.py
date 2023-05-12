57404faa18c014189fc8c23a51aca81a99b8e3f4
Esteban Odetti <estebanode@gmail.com>
2023-05-12 08:07:49 -0300

    CNNregresion

--- Redes/RedCNN.py
+++ Redes/RedCNN.py
@@ -37,27 +37,23 @@ for i in range(10):
 plt.tight_layout()
 plt.show()  
 
-class NetCNN(nn.Module):
-
+class CNNRegressor(nn.Module):
     def __init__(self):
-        super(NetCNN, self).__init__()
-        # las imagenes son de 28x28
-        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
-        self.pool = nn.MaxPool2d(2,2)
-        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
-        self.pool = nn.MaxPool2d(2,2)
-        self.fc1 = nn.Linear(16 * 4 * 4, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        # Al pasar de capa convolucional a capa totalmente conectada, tenemos
-        # que reformatear la salida para que se transforme en un vector unidimensional
-        x = x.view(-1, 16 * 4 * 4)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        super(CNNRegressor, self).__init__()
         
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.fc = nn.Linear(32 * 3 * 3, 1)  # Salida lineal para regresión
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
