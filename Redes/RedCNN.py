a0adcba307846c658adf96335a3eb02269e1b8fe
AndresPoz <andres-pozzer@hotmail.com>
2023-05-12 10:52:41 -0300

    Creamos las deniciones y el bucle de entrenamiento

--- Redes/RedCNN.py
+++ Redes/RedCNN.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 mat_fname = 'Datasets/mi_matriz.mat'
 mat = sio.loadmat(mat_fname)
 matriz_cargada = mat['dataset_matriz']
+from torch.utils.data import TensorDataset, DataLoader
 
 
 #primero mezclamos los casos
@@ -27,21 +28,21 @@ temp_test = matriz_cargada_mezclada[138:,:,:,17]
 
 primeros_10_casos = temp_train[:10]
 #intento de dibujo; NOSE SI ESTA BIEN
-for i in range(10):
-    caso = primeros_10_casos[i]
-    imagen = caso[:, :]  
-    plt.subplot(2, 5, i + 1)  
-    plt.imshow(imagen, cmap='gray')  
-    plt.axis('off')  
-    plt.title(f'Caso {i+1}')  
-plt.tight_layout()
-plt.show()  
+# for i in range(10):
+#     caso = primeros_10_casos[i]
+#     imagen = caso[:, :]  
+#     plt.subplot(2, 5, i + 1)  
+#     plt.imshow(imagen, cmap='gray')  
+#     plt.axis('off')  
+#     plt.title(f'Caso {i+1}')  
+# plt.tight_layout()
+# plt.show()  
 
 class CNNRegressor(nn.Module):
     def __init__(self):
         super(CNNRegressor, self).__init__()
         
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(20, 16, kernel_size=3, stride=1, padding=1)
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
@@ -57,3 +58,48 @@ class CNNRegressor(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
+net=CNNRegressor()
+# Tasa de aprendizaje inicial para el gradiente descendente
+learning_rate = 0.0001
+# Construimos el optimizador, y le indicamos que los parámetros a optimizar 
+# son los del modelo definido: net.parameters()
+optimmizer=torch.optim.Adam(net.parameters(),lr=learning_rate)
+# Definimos también la función de pérdida a utilizar
+criterion = torch.nn.CrossEntropyLoss() 
+# Creamos un loader iterable indicandole que debe leer los datos a partir de
+# del dataset creado en el paso anterior. Este objeto puede ser iterado
+loader = DataLoader(dataset=train, batch_size=3, shuffle=True)
+# Número de épocas
+num_epochs = 5
+# Lista en la que iremos guardando el valor de la función de pérdida en cada 
+# etapa de entrenamiento
+loss_list = []
+
+
+# Bucle de entrenamiento
+for i in range(num_epochs):
+
+    # Itero sobre todos los batches del dataset
+    for x, y in loader:
+        # Seteo en cero los gradientes de los parámetros a optimizar
+        optimmizer.zero_grad()
+
+        # Movemos los tensores a memoria de GPU, no hay GPU por ahora cpu
+       # x = x.to(device)
+       # y = y.to(device)
+       
+        # Realizo la pasada forward por la red
+        loss = criterion(net(x), y)
+        
+        # Realizo la pasada backward por la red        
+        loss.backward()
+        
+        # Actualizo los pesos de la red con el optimizador
+        optimmizer.step()
+
+        # Me guardo el valor actual de la función de pérdida para luego graficarlo
+        loss_list.append(loss.data.item())
+
+    # Muestro el valor de la función de pérdida cada 100 iteraciones        
+    #if i > 0 and i % 100 == 0:
+    print('Epoch %d, loss = %g' % (i, loss))
