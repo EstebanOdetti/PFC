f34b44e348655975f3d2182cd26eaae65e34e24d
Esteban Odetti <estebanode@gmail.com>
2023-05-12 17:27:19 -0300

    CNN error de tamaño

--- Redes/RedCNN.py
+++ Redes/RedCNN.py
@@ -23,12 +23,19 @@ num_casos, _, _, _ = matriz_cargada.shape
 indices_aleatorios = np.random.permutation(num_casos)
 matriz_cargada_mezclada = matriz_cargada[indices_aleatorios]
 # Puedo hacer 138 casos de train y 58 de train (30%)
-train = matriz_cargada_mezclada[0:137,:,:,1:16]
-test = matriz_cargada_mezclada[138:,:,:,1:16]
+train = matriz_cargada_mezclada[0:137,:,:,0:16]
+test = matriz_cargada_mezclada[138:,:,:,0:16]
 temp_train = matriz_cargada_mezclada[0:137,:,:,17]
 temp_test = matriz_cargada_mezclada[138:,:,:,17]
+print(train.shape)
+train_tensor = torch.from_numpy(train).float()
+test_tensor = torch.from_numpy(test).float()
+temp_train_tensor = torch.from_numpy(temp_train).float()
+temp_test_tensor = torch.from_numpy(temp_test).float()
 
-primeros_10_casos = temp_train[:10]
+train_tensor_reshape = train_tensor.permute(2, 0, 1).contiguous()
+
+#primeros_10_casos = temp_train[:10]
 #intento de dibujo; NOSE SI ESTA BIEN
 # for i in range(10):
 #     caso = primeros_10_casos[i]
@@ -44,11 +51,11 @@ class CNNRegressor(nn.Module):
     def __init__(self):
         super(CNNRegressor, self).__init__()
         
-        self.conv1 = nn.Conv2d(20, 16, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1)
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-        self.fc = nn.Linear(32 * 3 * 3, 1)  # Salida lineal para regresión
+        self.fc = nn.Linear(32 * height * width, 1)  # Ajustar el tamaño de entrada para self.fc
         
     def forward(self, x):
         x = self.conv1(x)
@@ -60,9 +67,35 @@ class CNNRegressor(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()  
+        # Capas convolucionales
+        self.conv1 = nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=0)
+        # Capa de agrupación (max pooling)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        # Capa completamente conectada
+        self.fc = nn.Linear(32 * 2 * 2, 64)
+        
+    def forward(self, x):
+        # Paso a través de la capa convolucional
+        x = self.conv1(x)
+        # Aplicar función de activación
+        x = torch.relu(x)
+        # Aplicar capa de agrupación
+        x = self.pool(x)
+        # Aplanar la salida de la capa de agrupación
+        x = x.view(x.size(0), -1)  # Corrección aquí
+        # Paso a través de la capa completamente conectada
+        x = self.fc(x)
+        return x
+
 # Dispositivo en que se ejecturá el modelo: 'cuda:0' para GPU y 'cpu' para CPU
-device = torch.device('cpu')
-net=CNNRegressor()
+#device = torch.device('cpu')
+device = torch.device('cuda:0')
+net=CNN()
+net = net.to(device)
 # Tasa de aprendizaje inicial para el gradiente descendente
 learning_rate = 0.0001
 # Construimos el optimizador, y le indicamos que los parámetros a optimizar 
@@ -74,56 +107,56 @@ criterion = torch.nn.CrossEntropyLoss()
 # del dataset creado en el paso anterior. Este objeto puede ser iterado
 ##Pero para nuestro caso tenemos que definir el dataset
 #class CustomDataset(Dataset):
-#    def __init__(self, data):
-#        self.data = data
-#        
-#    def __getitem__(self, index):
-#        x = self.data[index, 0]
-#        y = self.data[index, 1:]
-#        return x, y
-#    
-#    def __len__(self):
-#        return self.data.shape[0]
+ #   def __init__(self, data):
+  #      self.data = data
+        
+   # def __getitem__(self, index):
+    #    x = torch.from_numpy(self.data[index, :,:,0:16])
+     #   y = torch.from_numpy(self.data[index, :,:,17])
+      #  return x.to(device), y.to(device) 
+   
+    #def __len__(self):
+     #   return self.data.shape
+
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #loader = CustomDataset(train)
-loader = DataLoader(dataset=train, batch_size=6, shuffle=True)
-print(len(loader))
-print(len(loader.dataset))
+#loader = DataLoader(dataset=train, batch_size=6, shuffle=True)
+#print(len(loader))
+#print(len(loader.dataset))
 # Número de épocas
 num_epochs = 5
 # Lista en la que iremos guardando el valor de la función de pérdida en cada 
 # etapa de entrenamiento
 loss_list = []
-
-
 # Bucle de entrenamiento
 for i in range(num_epochs):
     #print(loader.data[i,1,1,0])
     # Itero sobre todos los batches del dataset
     
-    for x in loader:
+    for j in range(136):
         # Seteo en cero los gradientes de los parámetros a optimizar
         
         optimmizer.zero_grad()
-        
+        x = train_tensor[j]
+        y = temp_train_tensor[j]
         # Movemos los tensores a memoria de GPU o CPU
         x = x.to(device)
+        y = y.to(device)
         ##quiero ver la forma de cada elemento
-
-        print(x[0].shape)
-        
-        #y = y.to(device)
+        print(x.shape)
+        print(y.shape)
        
         # Realizo la pasada forward por la red
-        #loss = criterion(net(x), y)
+        loss = criterion(net(x), y)
         
         # Realizo la pasada backward por la red        
-        #loss.backward()
+        loss.backward()
         
         # Actualizo los pesos de la red con el optimizador
-        #optimmizer.step()
+        optimmizer.step()
 
         # Me guardo el valor actual de la función de pérdida para luego graficarlo
-        #loss_list.append(loss.data.item())
+        loss_list.append(loss.data.item())
 
     # Muestro el valor de la función de pérdida cada 100 iteraciones        
     #if i > 0 and i % 100 == 0:
