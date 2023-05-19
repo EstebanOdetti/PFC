c25747fd3a67a033ac245ca7a652a76eb8b13ddd
AndresPoz <andres-pozzer@hotmail.com>
2023-05-19 08:38:13 -0300

    Normalizacion anda

--- Redes/RedCNN2_norma.py
+++ Redes/RedCNN2_norma.py
@@ -129,9 +129,9 @@ train_dataset_normalized = TensorDataset(train_tensor_normalized.permute(0, 3, 1
 
 train_dataset_EO_normalized = TensorDataset(train_tensor_EO_normalized.permute(0, 3, 1, 2), temp_train_tensor_EO_normalized) # Reordenamos las dimensiones aquí
 
-test_dataset = TensorDataset(test_tensor_normalized.permute(0, 3, 1, 2), temp_test_tensor_normalized) # Reordenamos las dimensiones aquí
+test_dataset_normalized = TensorDataset(test_tensor_normalized.permute(0, 3, 1, 2), temp_test_tensor_normalized) # Reordenamos las dimensiones aquí
 
-test_dataset_EO = TensorDataset(test_tensor_EO_normalized.permute(0, 3, 1, 2), temp_test_tensor_EO_normalized) # Reordenamos las dimensiones aquí
+test_dataset_EO_normalized = TensorDataset(test_tensor_EO_normalized.permute(0, 3, 1, 2), temp_test_tensor_EO_normalized) # Reordenamos las dimensiones aquí
 
 # Creamos DataLoaders a partir de los TensorDatasets
 batch_size = 32  # Define el tamaño del batch que desees
@@ -143,6 +143,16 @@ train_loader_EO = DataLoader(train_dataset_EO, batch_size=batch_size, shuffle=Tr
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
 test_loader_EO = DataLoader(test_dataset_EO, batch_size=batch_size, shuffle=True)
+
+train_loader_normalized = DataLoader(train_dataset_normalized, batch_size=batch_size, shuffle=True)
+
+train_loader_EO_normalized = DataLoader(train_dataset_EO_normalized, batch_size=batch_size, shuffle=True)
+
+test_loader_normalized = DataLoader(test_dataset_normalized, batch_size=batch_size, shuffle=True)
+
+test_loader_EO_normalized = DataLoader(test_dataset_EO_normalized, batch_size=batch_size, shuffle=True)
+
+############# SIN NORMALIZAR
 
 class CNN(nn.Module):
     def __init__(self):
@@ -167,7 +177,7 @@ net = net.to(device)
 learning_rate = 0.0001
 optimizer=torch.optim.Adam(net.parameters(),lr=learning_rate)
 criterion = torch.nn.MSELoss() 
-num_epochs = 100
+num_epochs = 1500
 loss_list = []
 
 # Obtenemos el primer tensor del TensorDataset
@@ -236,4 +246,81 @@ with torch.no_grad():  # No necesitamos calcular los gradientes.
         mse_list.append(mse)
 
     print(f'MSE on test data: {np.mean(mse_list)}')
-    
+
+############# SDATOS NORMALIZADOS
+
+net_normalized=CNN()
+net_normalized = net.to(device)
+
+learning_rate = 0.0001
+optimizer=torch.optim.Adam(net_normalized.parameters(),lr=learning_rate)
+criterion = torch.nn.MSELoss() 
+num_epochs = 1500
+loss_list = []
+
+# Obtenemos el primer tensor del TensorDataset
+first_tensor = train_loader_normalized.dataset.tensors[0]
+# Imprimimos la forma del tensor
+print(first_tensor.size())
+# Bucle de entrenamiento
+for epoch in range(num_epochs):
+    for i, (x, y) in enumerate(train_loader_normalized):  # Ahora i es el índice de iteración
+        optimizer.zero_grad()
+
+        x = x.to(device)
+        y = y.to(device)
+
+        loss = criterion(net_normalized(x), y)
+        loss.backward()
+        optimizer.step()
+        loss_list.append(loss.item())
+        # Imprimimos cada 100 iteraciones
+        if epoch % 100 == 0:
+            print(f'Epoch {epoch}, Loss = {loss}')
+
+
+net.eval()  # Cambia el modo de la red a evaluación.
+
+with torch.no_grad():  # No necesitamos calcular los gradientes.
+    mse_list = []  # Lista para guardar los errores cuadráticos medios.
+
+    for i in range(len(test_tensor_normalized)):
+        inputs = test_tensor_normalized[i]
+        real_output = temp_test_tensor_normalized[i]
+
+        inputs = inputs.permute(2, 0, 1)  # Reordena las dimensiones a (canales, altura, ancho)
+        inputs = inputs.to(device)
+        real_output = real_output.to(device)
+
+
+        # Pasamos los datos por la red (forward pass)
+        predicted_output = net(inputs.unsqueeze(0)).squeeze()  # Asegúrate de eliminar la dimensión de batch con .squeeze()
+
+        # Calculamos la pérdida de validación
+        mse = mean_squared_error(real_output.cpu().detach().numpy(), predicted_output.cpu().detach().numpy())
+        mse_list.append(mse)
+
+    print(f'MSE on test data: {np.mean(mse_list)}')
+
+
+#####COMPROBAMOS QUE TAL FUNCIONA CON CONMUTAR ESTE OESTE
+with torch.no_grad():  # No necesitamos calcular los gradientes.
+    mse_list = []  # Lista para guardar los errores cuadráticos medios.
+
+    for i in range(len(test_tensor_EO_normalized)):
+        inputs = test_tensor_EO_normalized[i]
+        real_output = temp_test_tensor_EO_normalized[i]
+
+        inputs = inputs.permute(2, 0, 1)  # Reordena las dimensiones a (canales, altura, ancho)
+        inputs = inputs.to(device)
+        real_output = real_output.to(device)
+
+
+        # Pasamos los datos por la red (forward pass)
+        predicted_output = net(inputs.unsqueeze(0)).squeeze()  # Asegúrate de eliminar la dimensión de batch con .squeeze()
+
+        # Calculamos la pérdida de validación
+        mse = mean_squared_error(real_output.cpu().detach().numpy(), predicted_output.cpu().detach().numpy())
+        mse_list.append(mse)
+
+    print(f'MSE on test data: {np.mean(mse_list)}')
