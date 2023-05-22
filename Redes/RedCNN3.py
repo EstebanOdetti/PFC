3ee8a09ecc9d2294d8f390f59a053de35fee267c
Esteban Odetti <estebanode@gmail.com>
2023-05-22 18:56:00 -0300

    CNN3

--- Redes/RedCNN3.py
+++ Redes/RedCNN3.py
@@ -21,166 +21,118 @@ mat_fname = 'Datasets/mi_matriz_solo_diritletch'
 mat = sio.loadmat(mat_fname)
 matriz_cargada = mat['dataset_matriz']
 
-
-
 #primero mezclamos los casos
 num_casos, _, _, _ = matriz_cargada.shape
 indices_aleatorios = np.random.permutation(num_casos)
 matriz_cargada_mezclada = matriz_cargada[indices_aleatorios]
 # Puedo hacer 138 casos de train y 58 de train (30%)
-train = matriz_cargada_mezclada[0:137,:,:,0:16]
-test = matriz_cargada_mezclada[138:,:,:,0:16]
-temp_train = matriz_cargada_mezclada[0:137,:,:,17]
-temp_test = matriz_cargada_mezclada[138:,:,:,17]
-train_tensor = torch.from_numpy(train).float()
-test_tensor = torch.from_numpy(test).float()
+total_casos = matriz_cargada_mezclada.shape[0]
+porcentaje_entrenamiento = 0.7
+num_entrenamiento = int(total_casos * porcentaje_entrenamiento)
+num_pruebas = total_casos - num_entrenamiento
+#obtenermos las temperaturas en los bordes y en el interior es cero (Y ESTO ESS LO Q LE ENTRA A LA RED)
+temp_dirichlet_train = matriz_cargada_mezclada[:num_entrenamiento, :, :, 12]
+temp_dirichlet_test = matriz_cargada_mezclada[num_entrenamiento:, :, :, 12]
+# esto es el ground truth. 
+temp_train = matriz_cargada_mezclada[:num_entrenamiento, :, :, 17]
+temp_test = matriz_cargada_mezclada[num_entrenamiento:, :, :, 17]
+#convertis en tensores
+temp_dirichlet_train_tensor = torch.from_numpy(temp_dirichlet_train).float()
+temp_dirichlet_test_tensor = torch.from_numpy(temp_dirichlet_test).float()
 temp_train_tensor = torch.from_numpy(temp_train).float()
 temp_test_tensor = torch.from_numpy(temp_test).float()
-
-primeros_10_casos = temp_train_tensor[34:44]
-for i in range(10):
-    caso = primeros_10_casos[i]
-    imagen = caso[:, :]  
-    plt.subplot(2, 5, i + 1)  
-    plt.imshow(imagen, cmap='gray')  
-    plt.axis('off')  
-    plt.title(f'Caso {i+1}')  
-plt.tight_layout()
-plt.show()  
-
-# Preparar los datos de entrada
-input_data = np.zeros_like(train)
-input_data[:, :, 0, :] = train[:, :, 0, :]
-input_data[:, :, -1, :] = train[:, :, -1, :]
-input_data[:, 0, :, :] = train[:, 0, :, :]
-input_data[:, -1, :, :] = train[:, -1, :, :]
-
-input_tensor = torch.from_numpy(input_data).float().permute(0, 3, 1, 2)
-# Seleccionar el primer canal para graficarlo
-primer_canal = input_data[:, :, :, 0]
-# Graficar los primeros 10 casos del primer canal de los datos de entrada después de aplicar las condiciones de borde
-primeros_10_casos = primer_canal[34:44]
-for i in range(10):
-    caso = primeros_10_casos[i]
-    imagen = caso  # No se necesita [:, :]
-    plt.subplot(2, 5, i + 1)
-    plt.imshow(imagen, cmap='gray')
-    plt.axis('off')
-    plt.title(f'Caso {i+1}')
-plt.tight_layout()
-plt.show()
-# Preparar los datos de prueba
-test_data = np.zeros_like(test)
-test_data[:, :, 0, :] = test[:, :, 0, :]
-test_data[:, :, -1, :] = test[:, :, -1, :]
-test_data[:, 0, :, :] = test[:, 0, :, :]
-test_data[:, -1, :, :] = test[:, -1, :, :]
-test_tensor = torch.from_numpy(test_data).float().permute(0, 3, 1, 2)
-
+#mostras los 10 primeros casos
+#primeros_10_casos = temp_train_tensor[0:10]
+#for i in range(10):
+#    caso = primeros_10_casos[i]
+#    imagen = caso[:, :]
+#    plt.subplot(2, 5, i + 1)
+#    plt.imshow(imagen, cmap='hot')  # Utilizar cmap='hot' para representar temperaturas
+#    plt.title(f'Caso {i+1}')
+#plt.tight_layout()
+#plt.show()
+# Graficar los primeros 10 casos de solo borde
+#primeros_10_casos = temp_dirichlet_train_tensor[0:10]
+#for i in range(primeros_10_casos.shape[0]):
+#    caso = primeros_10_casos[i]
+#    imagen = caso  # No se necesita [:, :]
+#    plt.subplot(2, 5, i + 1)
+#    plt.imshow(imagen, cmap='hot')
+#    plt.title(f'Caso {i+1}')
+#plt.tight_layout()
+#plt.show()
+# Añadir una dimensión extra para los canales
+temp_dirichlet_train_tensor = temp_dirichlet_train_tensor.unsqueeze(1) # tamaño ahora es [num_entrenamiento, 1, height, width]
+temp_dirichlet_test_tensor = temp_dirichlet_test_tensor.unsqueeze(1) # tamaño ahora es [num_pruebas, 1, height, width]
+# Añadir una dimensión extra para los canales a los targets si es necesario
+temp_train_tensor = temp_train_tensor.unsqueeze(1)
+temp_test_tensor = temp_test_tensor.unsqueeze(1)
 # Crear TensorDatasets y DataLoaders
-train_dataset = TensorDataset(input_tensor, temp_train_tensor)
-test_dataset = TensorDataset(test_tensor, temp_test_tensor)
+train_dataset = TensorDataset(temp_dirichlet_train_tensor, temp_train_tensor)
+test_dataset = TensorDataset(temp_dirichlet_test_tensor, temp_test_tensor)
 batch_size = 32
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
-def custom_loss(y_pred, y_true, input_data):
-    # Calcular el error de reconstrucción cuadrático
-    reconstruction_error = torch.nn.MSELoss()(y_pred, y_true)
-
-    # Calcular la penalización de las condiciones de borde
-    border_penalty = torch.sum((y_pred[:, :, 0, :] - input_data[:, :, 0, :])**2)
-    border_penalty += torch.sum((y_pred[:, :, -1, :] - input_data[:, :, -1, :])**2)
-    border_penalty += torch.sum((y_pred[:, 0, :, :] - input_data[:, 0, :, :])**2)
-    border_penalty += torch.sum((y_pred[:, -1, :, :] - input_data[:, -1, :, :])**2)
-
-    # Combinar los dos componentes de la pérdida
-    loss = reconstruction_error + border_penalty
-
-    return loss
-
-class CNN(nn.Module):
+class HeatPropagationNet(nn.Module):
     def __init__(self):
-        super(CNN, self).__init__()  
-        
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.Dropout(0.5)
-        )
-        
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.Dropout(0.5)
-        )
-
-        self.conv3 = nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1)
+        super(HeatPropagationNet, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, stride=1, padding=1)
+        self.dropout = nn.Dropout2d(p=0.2)  # Añade una capa de Dropout. 'p' es la probabilidad de que cada nodo se apague.
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        return x
-    
-# Definir el dispositivo en el que se entrenará la red
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        x1 = self.dropout(F.leaky_relu(self.conv1(x)))
+        x2 = self.dropout(F.leaky_relu(self.conv2(x1)))
+        x3 = self.dropout(F.leaky_relu(self.conv3(x2)))
+        x4 = self.conv4(x3)  # Normalmente no aplicamos Dropout en la capa de salida
+        return x1, x2, x3, x4
 
-# Mover la red al dispositivo
-net = CNN().to(device)
 
-# Definir el optimizador
-optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)
+def custom_loss(outputs, target, ponderacion_interior, ponderacion_frontera):
+    loss_borde = 0
+    loss_interior = 0
+    for i, output in enumerate(outputs):
+        # calculamos la pérdida en los bordes sumando todos los canales
+        border_loss = F.mse_loss(output, target)
+        loss_borde += border_loss
 
-# Definir el número de épocas
-num_epochs = 2000
+        # calculamos la pérdida en el interior solo para la última capa
+        if i == len(outputs) - 1:
+            interior_loss = F.mse_loss(output[..., 1:-1, 1:-1], target[..., 1:-1, 1:-1])
+            loss_interior += interior_loss
+
+    loss_borde /= len(outputs)  # Normalizamos la pérdida en los bordes
+
+    return ponderacion_frontera * loss_borde + ponderacion_interior * loss_interior
+
+
+# Verifica si CUDA está disponible y selecciona el dispositivo
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Inicializar la red y el optimizador
+model = HeatPropagationNet().to(device)
+optimizer = optim.Adam(model.parameters(), lr = 0.001)
+
+# Parámetros para la función de pérdida
+ponderacion_interior = 0.6
+ponderacion_frontera = 0.4
 
 # Bucle de entrenamiento
-for epoch in range(num_epochs):
-    for i, (x, y) in enumerate(train_loader):  # Ahora i es el índice de iteración
-        # Mover los datos al dispositivo
-        x = x.to(device)
-        y = y.to(device)
+for epoch in range(10000):
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
 
-        # Pasar los datos a través de la red
-        output = net(x)
-
-        # Calcular la pérdida
-        loss = custom_loss(output, y, x)
-
-        # Retropropagar el error y actualizar los pesos
+        # Cero los gradientes del optimizador
         optimizer.zero_grad()
+
+        # Pase adelante, cálculo de la pérdida, pase atrás y optimización
+        outputs = model(inputs)
+        loss = custom_loss(outputs, labels, ponderacion_interior, ponderacion_frontera)
         loss.backward()
         optimizer.step()
 
-        # Imprimir la pérdida cada 100 épocas
-        if epoch % 100 == 0:
-            print(f'Epoch {epoch}, Loss = {loss.item()}')
-
-# Mover la red al modo de evaluación
-net.eval()
-
-# Inicializar la pérdida total
-total_loss = 0.0
-
-# Bucle de prueba
-with torch.no_grad():  # No necesitamos calcular gradientes en el bucle de prueba
-    for i, (x, y) in enumerate(test_loader):
-        # Mover los datos al dispositivo
-        x = x.to(device)
-        y = y.to(device)
-
-        # Pasar los datos a través de la red
-        output = net(x)
-
-        # Calcular la pérdida
-        loss = custom_loss(output, y, x)
-
-        # Acumular la pérdida total
-        total_loss += loss.item()
-
-# Calcular la pérdida media
-average_loss = total_loss / len(test_loader)
-
-print(f'Test Loss = {average_loss}')
+    # Imprime estadísticas de entrenamiento
+    print('Epoch: %d, Loss: %.3f' % (epoch + 1, loss.item()))
