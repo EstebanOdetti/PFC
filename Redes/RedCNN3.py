f285803cadc7e585e2aae87c4daa8003abeb8f14
Esteban Odetti <estebanode@gmail.com>
2023-05-29 17:30:11 -0300

    cambios

--- Redes/RedCNN3.py
+++ Redes/RedCNN3.py
@@ -54,15 +54,15 @@ temp_test_tensor = torch.from_numpy(temp_test).float()
 #plt.tight_layout()
 #plt.show()
 # Graficar los primeros 10 casos de solo borde
-primeros_10_casos = temp_dirichlet_train_tensor[0:10]
-for i in range(primeros_10_casos.shape[0]):
-    caso = primeros_10_casos[i]
-    imagen = caso  # No se necesita [:, :]
-    plt.subplot(2, 5, i + 1)
-    plt.imshow(imagen, cmap='hot')
-    plt.title(f'Caso {i+1}')
-plt.tight_layout()
-plt.show()
+#primeros_10_casos = temp_dirichlet_train_tensor[0:10]
+#for i in range(primeros_10_casos.shape[0]):
+#    caso = primeros_10_casos[i]
+#    imagen = caso  # No se necesita [:, :]
+#    plt.subplot(2, 5, i + 1)
+#    plt.imshow(imagen, cmap='hot')
+#    plt.title(f'Caso {i+1}')
+#plt.tight_layout()
+#plt.show()
 # Añadir una dimensión extra para los canales
 temp_dirichlet_train_tensor = temp_dirichlet_train_tensor.unsqueeze(1) # tamaño ahora es [num_entrenamiento, 1, height, width]
 temp_dirichlet_test_tensor = temp_dirichlet_test_tensor.unsqueeze(1) # tamaño ahora es [num_pruebas, 1, height, width]
@@ -78,10 +78,12 @@ test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 class HeatPropagationNet(nn.Module):
     def __init__(self):
         super(HeatPropagationNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(3,3), stride=1, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3,3), stride=1, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3,3), stride=1, padding=1)
-        self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3), stride=1, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(3,3), stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=(3,3), stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=16, kernel_size=(3,3), stride=1, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=16, out_channels=8, kernel_size=(3,3), stride=1, padding=1)
+        self.conv5 = nn.Conv2d(in_channels=8, out_channels=4, kernel_size=(3,3), stride=1, padding=1)
+        self.conv6 = nn.Conv2d(in_channels=4, out_channels=1, kernel_size=(3,3), stride=1, padding=1)
         #self.dropout = nn.Dropout2d(p=0.2)  # Añade una capa de Dropout. 'p' es la probabilidad de que cada nodo se apague.
 
     def forward(self, x):
@@ -91,8 +93,10 @@ class HeatPropagationNet(nn.Module):
         x2 = F.relu(self.conv2(x1))
         #x3 = self.dropout(F.leaky_relu(self.conv3(x2)))
         x3 = F.relu(self.conv3(x2))
-        x4 = self.conv4(x3)  # Normalmente no aplicamos Dropout en la capa de salida
-        return x1, x2, x3, x4
+        x4 = F.relu(self.conv4(x3))
+        x5 = F.relu(self.conv5(x4))
+        x6 = self.conv6(x5)  # Normalmente no aplicamos Dropout en la capa de salida
+        return x1, x2, x3, x4, x5, x6
 
 
 def custom_loss(outputs, target, ponderacion_interior, ponderacion_frontera):
@@ -107,7 +111,8 @@ def custom_loss(outputs, target, ponderacion_interior, ponderacion_frontera):
             feature_map = output[:, i, :, :].unsqueeze(1)  # Selecciona el mapa de características y añade una dimensión de canal
 
             # Calcula la pérdida en los bordes
-            border_loss = F.mse_loss(feature_map, target)
+            border_loss = F.mse_loss(feature_map[:, :, :, [0, -1]], target[:, :, :, [0, -1]]) + \
+                          F.mse_loss(feature_map[:, :, [0, -1], :], target[:, :, [0, -1], :])
             loss_borde_total += border_loss
 
             # Calcula la pérdida en el interior solo para la última capa
@@ -165,8 +170,8 @@ model = HeatPropagationNet().to(device)
 optimizer = optim.Adam(model.parameters(), lr = 0.001)
 
 # Parámetros para la función de pérdida
-ponderacion_interior = 0.4
-ponderacion_frontera = 0.6
+ponderacion_interior = 0.1
+ponderacion_frontera = 0.9
 
 # Bucle de entrenamiento
 for epoch in range(10000):
@@ -178,34 +183,37 @@ for epoch in range(10000):
         optimizer.zero_grad()
 
         # Pase adelante, cálculo de la pérdida, pase atrás y optimización
-        feature_maps1, feature_maps2, feature_maps3, output = model(inputs)
-        outputs = [feature_maps1, feature_maps2, feature_maps3, output]
+        feature_maps1, feature_maps2, feature_maps3, feature_maps4, feature_maps5, output = model(inputs)
+        outputs = [feature_maps1, feature_maps2, feature_maps3,feature_maps4, feature_maps5, output]
+        outputs_2 = [output]
         loss = custom_loss(outputs, labels, ponderacion_interior, ponderacion_frontera)
+        loss_2 = custom_loss(outputs_2, labels, ponderacion_interior, ponderacion_frontera)
         loss.backward()
         optimizer.step()
 
     # Imprime estadísticas de entrenamiento
-    print('Epoch: %d, Loss: %.3f' % (epoch + 1, loss.item()))
+    print('Epoch: %d, Loss_total: %.3f' % (epoch + 1, loss.item()))
+    print('Epoch: %d, Loss_ultima_capa: %.3f' % (epoch + 1, loss_2.item()))
 
     # Visualizar los mapas de características después de cada época
-    if epoch % 100 == 0:  # ajusta este número para visualizar los mapas de características con la frecuencia que desees
-        plot_feature_maps(feature_maps1)
-        plot_feature_maps(feature_maps2)
-        plot_feature_maps(feature_maps3)
+    #if epoch % 100 == 0:  # ajusta este número para visualizar los mapas de características con la frecuencia que desees
+        #plot_feature_maps(feature_maps1)
+        #plot_feature_maps(feature_maps2)
+        #plot_feature_maps(feature_maps3)
 
         # Visualizar los kernels de las capas convolucionales
-        kernels1 = model.conv1.weight.detach().clone()
-        kernels1 = kernels1 - kernels1.min()
-        kernels1 = kernels1 / kernels1.max()
-        plot_kernels(kernels1)
+        #kernels1 = model.conv1.weight.detach().clone()
+        #kernels1 = kernels1 - kernels1.min()
+        #kernels1 = kernels1 / kernels1.max()
+        #plot_kernels(kernels1)
 
-        kernels2 = model.conv2.weight.detach().clone()
-        kernels2 = kernels2 - kernels2.min()
-        kernels2 = kernels2 / kernels2.max()
-        plot_kernels(kernels2)
+        #kernels2 = model.conv2.weight.detach().clone()
+        #kernels2 = kernels2 - kernels2.min()
+        #kernels2 = kernels2 / kernels2.max()
+        #plot_kernels(kernels2)
 
-        kernels3 = model.conv3.weight.detach().clone()
-        kernels3 = kernels3 - kernels3.min()
-        kernels3 = kernels3 / kernels3.max()
-        plot_kernels(kernels3)
+        #kernels3 = model.conv3.weight.detach().clone()
+        #kernels3 = kernels3 - kernels3.min()
+        #kernels3 = kernels3 / kernels3.max()
+        #plot_kernels(kernels3)
 
