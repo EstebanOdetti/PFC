d35e9556f102c07a24e8e97f734ed3d6207e8670
Esteban Odetti <estebanode@gmail.com>
2023-06-11 17:31:09 -0300

    Graficas y KFOLD

--- Redes/RedCNN6.py
+++ Redes/RedCNN6.py
@@ -18,9 +18,8 @@ from torch.utils.data import Dataset, DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from sklearn.metrics import r2_score
 import shutil 
-#Borramos los runs anteriores
-shutil.rmtree('runs')
-mat_fname = 'Datasets/mi_matriz_solo_diritletch_enriquesida'
+from sklearn.model_selection import KFold
+mat_fname = 'Datasets/mi_matriz_solo_diritletch_enriquesida.mat'
 mat = sio.loadmat(mat_fname)
 matriz_cargada = mat['dataset_matriz']
 
@@ -33,27 +32,22 @@ total_casos = matriz_cargada_mezclada.shape[0]
 porcentaje_entrenamiento = 0.7
 num_entrenamiento = int(total_casos * porcentaje_entrenamiento)
 num_pruebas = total_casos - num_entrenamiento
-#obtenermos las temperaturas en los bordes y en el interior es cero (Y ESTO ESS LO Q LE ENTRA A LA RED)
-temp_dirichlet_train = matriz_cargada_mezclada[:num_entrenamiento, :, :, 12]
-temp_dirichlet_test = matriz_cargada_mezclada[num_entrenamiento:, :, :, 12]
 # esto es el ground truth. 
 temp_train = matriz_cargada_mezclada[:num_entrenamiento, :, :, 17]
 temp_test = matriz_cargada_mezclada[num_entrenamiento:, :, :, 17]
 #convertis en tensores
-temp_dirichlet_train_tensor = torch.from_numpy(temp_dirichlet_train).float()
-temp_dirichlet_test_tensor = torch.from_numpy(temp_dirichlet_test).float()
 temp_train_tensor = torch.from_numpy(temp_train).float()
 temp_test_tensor = torch.from_numpy(temp_test).float()
 #mostras los 10 primeros casos
-#primeros_10_casos = temp_train_tensor[0:10]
-#for i in range(10):
-#    caso = primeros_10_casos[i]
-#    imagen = caso[:, :]
-#    plt.subplot(2, 5, i + 1)
-#    plt.imshow(imagen, cmap='hot')  # Utilizar cmap='hot' para representar temperaturas
-#    plt.title(f'Caso {i+1}')
-#plt.tight_layout()
-#plt.show()
+primeros_10_casos = temp_train_tensor[0:10]
+for i in range(10):
+    caso = primeros_10_casos[i]
+    imagen = caso[:, :]
+    plt.subplot(2, 5, i + 1)
+    plt.imshow(imagen, cmap='hot')  # Utilizar cmap='hot' para representar temperaturas
+    plt.title(f'Caso {i+1}')
+plt.tight_layout()
+plt.show()
 # Graficar los primeros 10 casos de solo borde
 #primeros_10_casos = temp_dirichlet_train_tensor[0:10]
 #for i in range(primeros_10_casos.shape[0]):
@@ -64,18 +58,13 @@ temp_test_tensor = torch.from_numpy(temp_test).float()
 #    plt.title(f'Caso {i+1}')
 #plt.tight_layout()
 #plt.show()
-# Añadir una dimensión extra para los canales
-temp_dirichlet_train_tensor = temp_dirichlet_train_tensor.unsqueeze(1) # tamaño ahora es [num_entrenamiento, 1, height, width]
-temp_dirichlet_test_tensor = temp_dirichlet_test_tensor.unsqueeze(1) # tamaño ahora es [num_pruebas, 1, height, width]
-# Añadir una dimensión extra para los canales a los targets si es necesario
+# Añadir una dimensión extra para los canales a los targets
 temp_train_tensor = temp_train_tensor.unsqueeze(1)
 temp_test_tensor = temp_test_tensor.unsqueeze(1)
 # Crear TensorDatasets y DataLoaders
+# Aquí solo estamos considerando los datos de entrenamiento para la validación cruzada.
 train_dataset = TensorDataset(temp_train_tensor, temp_train_tensor)
 test_dataset = TensorDataset(temp_test_tensor, temp_test_tensor)
-batch_size = 32
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 class HeatPropagationNet(nn.Module):
     def __init__(self):
         super(HeatPropagationNet, self).__init__()
@@ -90,28 +79,28 @@ class HeatPropagationNet(nn.Module):
         # Bloque de salida
         self.conv3_1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)
         self.conv3_2 = nn.Conv2d(8, 1, kernel_size=3, padding=1)
-        self.dropout = nn.Dropout2d(p=0.2)  # Añade una capa de Dropout. 'p' es la probabilidad de que cada nodo se apague.
+        #self.dropout = nn.Dropout2d(p=0.01)  # Añade una capa de Dropout. 'p' es la probabilidad de que cada nodo se apague.
 
     def forward(self, x):
         #capa1
         x1 = F.relu(self.conv1_1(x))
-        y1 = self.dropout(self.conv1_2(x1))
-        #y1 = self.conv1_2(x1)
+        #y1 = self.dropout(self.conv1_2(x1))
+        y1 = self.conv1_2(x1)
         #capa2
         x2 = F.relu(self.conv2_1(y1))
-        y2 = self.conv2_2(x2)
+        y2 =  self.conv2_2(x2)
         #capa3
         x3 = F.relu(self.conv3_1(y2))
-        y3 = self.conv3_2(x3)
+        y3 =  self.conv3_2(x3)
         #capa4
         x4 = F.relu(self.conv1_1(y3))
-        y4 = self.conv1_2(x4)
+        y4 =  self.conv1_2(x4)
         #capa5
         x5 = F.relu(self.conv2_1(y4))
-        y5 = self.conv2_2(x5)
+        y5 =  self.conv2_2(x5)
         #capa6
         x6 = F.relu(self.conv3_1(y5))
-        y6 = self.conv3_2(x6)
+        y6 =  self.conv3_2(x6)
         return y1, y2, y3, y3, y4, y5, y6
 
 def custom_loss(outputs, target):
@@ -122,59 +111,6 @@ def custom_loss(outputs, target):
 
     return loss_tot
 
-
-#plt.ion()  # Habilita el modo interactivo
-
-# Crea las figuras y los ejes que se actualizarán durante el entrenamiento
-#fig_ground_truth, ax_ground_truth = plt.subplots(1, 2)
-#fig_feature_map, ax_feature_map = plt.subplots()
-"""
-def show_ground_truth(img, label, fig, ax):
-    img = img.cpu().numpy().squeeze()
-    label = label.cpu().numpy().squeeze()
-
-    ax[0].imshow(img)
-    ax[0].title.set_text('Input Image')
-    ax[1].imshow(label)
-    ax[1].title.set_text('Ground Truth')
-    
-    fig.canvas.draw()  
-    fig.canvas.flush_events()
-
-def plot_feature_maps(feature_maps, fig, ax, num_cols=6):
-    num_kernels = feature_maps.shape[1]
-    num_rows = 1+ num_kernels // num_cols
-    fig.set_size_inches(num_cols,num_rows)
-    fig.clf()
-    for i in range(num_kernels):
-        ax1 = fig.add_subplot(num_rows,num_cols,i+1)
-        ax1.imshow(feature_maps[0, i].cpu().detach().numpy())
-        ax1.axis('off')
-        ax1.set_xticklabels([])
-        ax1.set_yticklabels([])
-
-    plt.subplots_adjust(wspace=0.1, hspace=0.1)
-    fig.canvas.draw()  
-    fig.canvas.flush_events()
-    
-def plot_kernels(tensor, num_cols=6):
-    if not tensor.ndim==4:
-        raise Exception("assumes a 4D tensor")
-    if not tensor.shape[-1]==3:
-        raise Exception("last dim needs to be 3 to plot")
-    num_kernels = tensor.shape[0]
-    num_rows = 1+ num_kernels // num_cols
-    fig = plt.figure(figsize=(num_cols,num_rows))
-    for i in range(tensor.shape[0]):
-        ax1 = fig.add_subplot(num_rows,num_cols,i+1)
-        ax1.imshow(tensor[i].cpu().numpy())
-        ax1.axis('off')
-        ax1.set_xticklabels([])
-        ax1.set_yticklabels([])
-
-    plt.subplots_adjust(wspace=0.1, hspace=0.1)
-    plt.show()
-"""
 # Inicializa el escritor de TensorBoard
 writer = SummaryWriter('runs/experiment_1')
 
@@ -185,98 +121,145 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = HeatPropagationNet().to(device)
 optimizer = optim.Adam(model.parameters(), lr = 0.001)
 
-# Parámetros para la función de pérdida
-ponderacion_interior = 0.5
-ponderacion_frontera = 0.5
+# Define los pliegues
+k_folds = 5
+kfold = KFold(n_splits=k_folds, shuffle=True)
+# Inicializar la mejor pérdida a un número muy alto
+best_val_loss = float('inf')
+best_model_state = None
 
-# Bucle de entrenamiento
-for epoch in range(5000):
-    for i, data in enumerate(train_loader, 0):
-        inputs, labels = data
-        inputs, labels = inputs.to(device), labels.to(device)
+for fold, (train_index, val_index) in enumerate(kfold.split(train_dataset)):
+    # Dividir los datos de entrenamiento en conjuntos de entrenamiento y validación
+    train_subset = torch.utils.data.Subset(train_dataset, train_index)
+    val_subset = torch.utils.data.Subset(train_dataset, val_index)
 
-        # Cero los gradientes del optimizador
-        optimizer.zero_grad()
+    # Crea los DataLoader para los conjuntos de entrenamiento y validación
+    trainloader = torch.utils.data.DataLoader(train_subset, batch_size=32)
+    valloader = torch.utils.data.DataLoader(val_subset, batch_size=32)
 
-        # Pase adelante, cálculo de la pérdida, pase atrás y optimización
-        outputs = model(inputs)
-        outputs_all = outputs[:-1]
-        output_final = outputs[-1:]
-        loss_all = custom_loss(outputs_all, labels)
-        loss_final = custom_loss(output_final, labels)
+    # Aquí deberías inicializar y/o cargar tu modelo y optimizador
+    model = HeatPropagationNet().to(device)
+    optimizer = optim.Adam(model.parameters(), lr = 0.001)
+
+    # Bucle de entrenamiento y validación
+    for epoch in range(200):
+        train_loss_total, train_loss_ultima, val_loss_total, val_loss_ultima = 0.0, 0.0, 0.0, 0.0 
+        num_batches_train, num_batches_val = 0, 0
+        for inputs, labels in trainloader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            outputs_all = outputs[:-1]
+            output_final = outputs[-1:]
+            loss_all = custom_loss(outputs_all, labels)
+            loss_final = custom_loss(output_final, labels)
+            loss_all.backward()
+            optimizer.step()
+            train_loss_total += loss_all.item()
+            train_loss_ultima += loss_final.item()
+            num_batches_train += 1
+        with torch.no_grad():
+            for inputs, labels in valloader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                outputs_all = outputs[:-1]
+                output_final = outputs[-1:]
+                loss_all_validation = custom_loss(outputs_all, labels)
+                loss_ultima_validation = custom_loss(output_final, labels)
+                val_loss_total += loss_all_validation.item()
+                val_loss_ultima += loss_ultima_validation.item()
+                num_batches_val += 1
+        # Actualiza la mejor pérdida de validación y guarda el estado del modelo si es necesario
+        if val_loss_total < best_val_loss:
+            best_val_loss = val_loss_total
+            best_model_state = model.state_dict()
+        train_loss_total /= num_batches_train
+        train_loss_ultima /= num_batches_train
+        val_loss_total /= num_batches_val
+        val_loss_ultima /= num_batches_val
         
-        # Backward and optimization
-        loss_all.backward()
-        optimizer.step()
-    if epoch % 10 == 0:
-        print(f'Epoca {epoch}, Toda la loss: {loss_all.item()}')
-        print(f'Epoca {epoch}, Toda ultima capa: {loss_final.item()}')
+        if epoch % 50 == 0:
+            print(f'Fold {fold}, Epoch {epoch}, Training Loss (total): {train_loss_total}, Validation Loss: {val_loss_total}')
+            print(f'Fold {fold}, Epoch {epoch}, Training Loss (ultima capa): {train_loss_ultima}, Validation Loss: {val_loss_ultima}')
 
+model = HeatPropagationNet().to(device)
+model.load_state_dict(best_model_state)
 
-    # Añade las pérdidas a TensorBoard
-    writer.add_scalar('Loss_all_layers', loss_all.item(), epoch)
-    writer.add_scalar('Loss_final_layer', loss_final.item(), epoch)
+model = HeatPropagationNet().to(device)
+model.load_state_dict(best_model_state)
 
-    # Selecciona los primeros 3 mapas de características de cada capa y añade una dimensión de batch
-    feature_map1_single = outputs[0][0, :3].unsqueeze(0)
-    feature_map2_single = outputs[1][0, :3].unsqueeze(0)
-    feature_map3_single = outputs[2][0, :3].unsqueeze(0)
+# Crear DataLoader para el conjunto de pruebas
+test_dataset = TensorDataset(temp_test_tensor, temp_test_tensor)
+test_loader = DataLoader(test_dataset, batch_size=32)
 
-    # Asegura que los valores estén en el rango [0, 1]
-    feature_map1_single = (feature_map1_single - feature_map1_single.min()) / (feature_map1_single.max() - feature_map1_single.min())
-    feature_map2_single = (feature_map2_single - feature_map2_single.min()) / (feature_map2_single.max() - feature_map2_single.min())
-    feature_map3_single = (feature_map3_single - feature_map3_single.min()) / (feature_map3_single.max() - feature_map3_single.min())
-
-    writer.add_images('Feature Maps 1', feature_map1_single, epoch)
-    writer.add_images('Feature Maps 2', feature_map2_single, epoch)
-    writer.add_images('Feature Maps 3', feature_map3_single, epoch)
-
-    # Normaliza las entradas y las etiquetas
-    inputs_normalized = (inputs - inputs.min()) / (inputs.max() - inputs.min())
-    labels_normalized = (labels - labels.min()) / (labels.max() - labels.min())
-
-    writer.add_images('Input Images', inputs_normalized, epoch)
-    writer.add_images('Ground Truth', labels_normalized, epoch)
-
-
-writer.close()
-print("Cambiando a evaluacion...")
-model.eval()  # Cambia el modo del modelo a evaluación
+# Cambia el modelo a modo de evaluación
+model.eval()
 
 total_loss = 0
 num_batches = 0
 
-with torch.no_grad():  # No necesitamos calcular gradientes en el test
+# No necesitamos calcular gradientes durante la evaluación
+with torch.no_grad():
     for i, data in enumerate(test_loader, 0):
         inputs, labels = data
         inputs, labels = inputs.to(device), labels.to(device)
 
-        # Haz una predicción con el modelo
+        # Hacer una predicción con el modelo
         outputs = model(inputs)
 
-        # Calcula la pérdida
+        # Calcular la pérdida
         loss = custom_loss(outputs, labels)
         total_loss += loss.item()
         num_batches += 1
 
-        # Visualizar las primeras 5 salidas y ground truth
-        if i == 0:
-            for j in range(5):
-                plt.figure(figsize=(10,4))
-                
-                plt.subplot(1, 2, 1)
-                plt.imshow(outputs[-1][j, 0].cpu().numpy(), cmap='hot')
-                plt.title("Output de la red")
-
-                plt.subplot(1, 2, 2)
-                plt.imshow(labels[j, 0].cpu().numpy(), cmap='hot')
-                plt.title("Ground Truth")
-
-                plt.show()
-
-# Calcula las métricas medias
+# Calcular la pérdida media
 mean_loss = total_loss / num_batches
 
-print('Mean Loss:', mean_loss)
+print('Mean Loss on Test Set:', mean_loss)
 
+# Visualizar las salidas y ground truth una vez después de evaluar todo el conjunto de pruebas
+# Crear una figura con una cuadrícula de subplots
+fig, axs = plt.subplots(5, 2, figsize=(10, 20))
 
+# Recorrer las 5 salidas y ground truth
+for j in range(5):
+    # Mostrar la salida de la red en el primer subplot de la fila j
+    axs[j, 0].imshow(outputs[-1][j, 0].cpu().numpy(), cmap='hot')
+    axs[j, 0].set_title("Output de la red")
+
+    # Mostrar el ground truth en el segundo subplot de la fila j
+    axs[j, 1].imshow(labels[j, 0].cpu().numpy(), cmap='hot')
+    axs[j, 1].set_title("Ground Truth")
+
+# Ajustar los espacios entre subplots y entre las figuras
+fig.tight_layout()
+
+# Mostrar la figura
+plt.show()
+
+# Crear una figura y un eje para la gráfica
+plt.figure(figsize=(8, 6))
+ax = plt.gca()
+
+# Obtener los valores del ground truth y la salida de la red
+ground_truth = labels[:, 0].cpu().numpy().flatten()
+red_output = outputs[-1][:, 0].cpu().numpy().flatten()
+
+# Graficar los puntos del ground truth en rojo
+plt.scatter(red_output, ground_truth, c='red', label='Ground Truth')
+
+# Graficar los puntos de la salida de la red en azul
+plt.scatter(red_output, red_output, c='blue', label='Salida de la red')
+
+# Establecer etiquetas de los ejes
+plt.xlabel("Salida de la red")
+plt.ylabel("Ground Truth")
+
+# Establecer título de la gráfica
+plt.title("Dispersión de los puntos")
+
+# Mostrar la leyenda
+plt.legend()
+
+# Mostrar la gráfica
+plt.show()
