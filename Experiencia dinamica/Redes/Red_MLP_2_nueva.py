05b98e694caf8944f5af3f44df96d8063fb88aa4
AndresPoz <andres-pozzer@hotmail.com>
2023-12-16 10:36:04 -0300

    con grafica

--- Experiencia dinamica/Redes/Red_MLP_2_nueva.py
+++ Experiencia dinamica/Redes/Red_MLP_2_nueva.py
@@ -1,3 +1,5 @@
+from matplotlib import pyplot as plt
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import os
@@ -20,7 +22,7 @@ mediciones_simulaciones = pd.read_csv(simulations_data_path)
 num_rows_per_case = 189
 num_cases = len(combined_data) // num_rows_per_case
 X = combined_data.iloc[:, [0, 1, 3, 4]].values.reshape(num_cases, num_rows_per_case * 4)
-y = mediciones_simulaciones.iloc[:, 1:].values
+y = mediciones_simulaciones.iloc[:, 1:5].values
 
 # Comprobar si las dimensiones de X y y son compatibles
 if X.shape[0] != y.shape[0]:
@@ -48,32 +50,42 @@ test_loader = DataLoader(test_dataset, batch_size=4, shuffle=False)
 
 # Definir la arquitectura de la red
 class MLP(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size1, hidden_size2, hidden_size3, output_size):
         super(MLP, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.fc1 = nn.Linear(input_size, hidden_size1)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size1, hidden_size2)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(hidden_size2, hidden_size3)
+        self.relu3 = nn.ReLU()
+        self.fc4 = nn.Linear(hidden_size3, output_size)
 
     def forward(self, x):
         x = self.fc1(x)
-        x = self.relu(x)
+        x = self.relu1(x)
         x = self.fc2(x)
+        x = self.relu2(x)
+        x = self.fc3(x)
+        x = self.relu3(x)
+        x = self.fc4(x)
         return x
 
 # Parámetros de la red
 input_size = 756  # Número de características de entrada
-hidden_size = 100  # Puedes ajustar esto
-output_size = 6    # Número de características de salida
+hidden_size1 = 100  # Tamaño de la primera capa oculta
+hidden_size2 = 50   # Tamaño de la segunda capa oculta
+hidden_size3 = 20   # Tamaño de la tercera capa oculta
+output_size = 4    # Número de características de salida
 
 # Instanciar la red
-model = MLP(input_size, hidden_size, output_size)
+model = MLP(input_size, hidden_size1, hidden_size2, hidden_size3, output_size)
 
 # Definir la función de pérdida y el optimizador
 criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.01)
 
 # Entrenamiento
-num_epochs = 10  # Puedes ajustar esto
+num_epochs = 150  # Puedes ajustar esto
 for epoch in range(num_epochs):
     for inputs, targets in train_loader:
         optimizer.zero_grad()
@@ -86,10 +98,37 @@ for epoch in range(num_epochs):
 # Evaluación
 model.eval()
 with torch.no_grad():
-    correct = 0
-    total = 0
+    predictions = []
+    errors_by_feature = [[] for _ in range(output_size)]
+    
     for inputs, targets in test_loader:
         outputs = model(inputs)
-        total += targets.size(0)
-        correct += (outputs.argmax(dim=1) == targets.argmax(dim=1)).sum().item()
-    print(f'Test Accuracy: {100 * correct / total}%')
+        predictions.append(outputs.numpy())
+        
+        batch_errors = ((outputs - targets)**2).mean(dim=0).sqrt().numpy()
+        
+        for i in range(output_size):
+            errors_by_feature[i].append(batch_errors[i])
+
+# Convertir las listas en arrays numpy
+predictions = np.concatenate(predictions, axis=0)
+y_test_np = y_test
+
+# Gráfica del ground truth vs predicciones en subgráficos
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 8))
+
+for i, ax in enumerate(axes.flatten()):
+    ax.scatter(y_test_np[:, i], predictions[:, i], label=f'Feature {i+1} (Predictions)', color='blue')
+    ax.scatter(y_test_np[:, i], y_test_np[:, i], label=f'Feature {i+1} (Ground Truth)', color='red', marker='x')
+    ax.set_title(f'Feature {i+1}')
+    ax.set_xlabel('Ground Truth')
+    ax.set_ylabel('Predictions')
+    ax.legend()
+
+plt.tight_layout()
+plt.show()
+
+# Calcular y mostrar el error medio por característica
+for i in range(output_size):
+    feature_error = np.mean(errors_by_feature[i])
+    print(f'Average Error for Feature {i+1}: {feature_error:.4f}')
