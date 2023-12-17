8f7f833ed55013452bc4c3c74c40137b5e8319d0
Esteban Odetti <estebanode@gmail.com>
2023-12-17 18:21:16 -0300

    red CNN2 y datasets bien ordenados

--- Experiencia dinamica/Generador de dataset/reordenar_dataset_CNN.py
+++ Experiencia dinamica/Generador de dataset/reordenar_dataset_CNN.py
@@ -1,48 +1,42 @@
 import pandas as pd
 import numpy as np
+import os
 
-def procesar_datos(file_path_x, file_path_y, output_file_path):
+
+def procesar_datos(file_path_x, output_file_path):
     # Cargar datos
-    data_x = pd.read_csv(file_path_x, delimiter=',')    
-    data_y = pd.read_csv(file_path_y, delimiter=',')
-    num_blocks_x = data_x.shape[0] // 189
-   # num_blocks_y = data_y.shape[0]
-    print(num_blocks_x)
-    # Inicializar una lista para almacenar las matrices transpuestas
-    transposed_matrices = []
+    data_x = pd.read_csv(file_path_x, delimiter=',')
+    num_blocks = data_x.shape[0] // 189
+
+    # Inicializar una lista para almacenar las matrices procesadas
+    processed_matrices = []
 
     # Procesar cada bloque de datos
-    for i in range(num_blocks_x):
+    for i in range(num_blocks):
         # Extraer las filas correspondientes al bloque
-        block_data_x = data_x.iloc[i*189 : (i+1)*189]
-        #block_data_y = data_y.iloc[i : (i+1)]
-        # Extraer las columnas de entrada
-        X = block_data_x[['Freq', 'PSD']].values
-        #Y = block_data_y[['P1_Frecuencia','P1_RMS']].values
-        
-        # Organizar los datos en una matriz de 4x30
-        X_reshape = np.empty((4, 63))
-        X_reshape[0, :] = X[:63, 0]
-        X_reshape[1, :] = X[:63, 1]
-        X_reshape[2, :] = X[63:126, 1]
-        X_reshape[3, :] = X[126:189, 1]
-       # X_reshape[4, :] = Y[0, 2]
-       # X_reshape[5, :] = Y[1, 3]
+        block = data_x.iloc[i*189 : (i+1)*189]
 
-        # Trasponer la matriz
-        X_transposed = X_reshape.T
+        # Extraer las columnas de Freq y Filename
+        Freq = block['Freq'].iloc[:63].values
+        Filename = block['Filename'].iloc[0]
 
-        # Añadir la matriz transpuesta a la lista
-        transposed_matrices.append(X_transposed)
+        # Extraer y organizar los datos de PSD para cada eje
+        PSD_X = block['PSD'].iloc[:63].values
+        PSD_Y = block['PSD'].iloc[63:126].values
+        PSD_Z = block['PSD'].iloc[126:189].values
 
-    # Concatenar las matrices transpuestas
-    final_matrix = np.vstack(transposed_matrices)
+        # Combinar las columnas en una matriz y añadirla a la lista
+        combined_matrix = np.column_stack([Freq, PSD_X, PSD_Y, PSD_Z, np.repeat(Filename, 63)])
+        processed_matrices.append(combined_matrix)
+
+    # Concatenar todas las matrices procesadas
+    final_matrix = np.vstack(processed_matrices)
 
     # Guardar la matriz final en un archivo CSV
-    np.savetxt(output_file_path, final_matrix, delimiter=',')
+    np.savetxt(output_file_path, final_matrix, delimiter=',', fmt='%s')
 
 # Usar la función
 file_path_x = 'C:/Users/Usuario/Desktop/Proyectos/PyTorch/PyThorch Test/Experiencia dinamica/Datasets/FINAL/combined_data.csv' 
-file_path_y= 'C:/Users/Usuario/Desktop/Proyectos/PyTorch/PyThorch Test/Experiencia dinamica/Datasets/FINAL/Mediciones_simulaciones.csv'  
+
 output_file_path = 'C:/Users/Usuario/Desktop/Proyectos/PyTorch/PyThorch Test/Experiencia dinamica/Datasets/matriz_total_ordenado_bien.csv'
-procesar_datos(file_path_x, file_path_y, output_file_path)
+procesar_datos(file_path_x, output_file_path)
---
+++ Experiencia dinamica/Redes/Red_CNN_2_nueva.py
@@ -0,0 +1,177 @@
+import pandas as pd
+import numpy as np
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.model_selection import train_test_split
+import os
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+
+#Directorio base donde se encuentran tus archivos
+directorio_base = os.path.dirname(__file__)  # Obtener el directorio del script actual
+
+# Leer y preparar el conjunto de datos
+matriz_total_data_path = os.path.join(directorio_base, 'Datasets', 'matriz_total.csv')
+simulations_data_path = os.path.join(directorio_base, 'Datasets\FINAL', 'Mediciones_simulaciones.csv')
+
+# Cargar datos
+matriz_total = pd.read_csv(matriz_total_data_path)
+mediciones_simulaciones = pd.read_csv(simulations_data_path)
+
+# Asegurar que las dimensiones son correctas
+assert matriz_total.shape[0] == 3654
+
+# Número de filas que representa un caso y número total de casos
+num_rows_per_case = 63
+num_cases = 29
+
+# Dividir matriz_total en dos: adelante (rueda delantera) y atrás (rueda trasera)
+half_index = len(matriz_total) // 2
+matriz_adelante = matriz_total.iloc[:half_index, :4]  # Seleccionar solo las primeras 4 columnas
+matriz_atras = matriz_total.iloc[half_index:, :4]    # Seleccionar solo las primeras 4 columnas
+
+# Redimensionar los datos para que tengan la forma (num_cases, 4, num_rows_per_case, 1)
+X_adelante = matriz_adelante.values.reshape(num_cases, 4, num_rows_per_case, 1)
+X_atras = matriz_atras.values.reshape(num_cases, 4, num_rows_per_case, 1)
+
+# Preparar las etiquetas (salidas) correspondientes a cada caso
+y = mediciones_simulaciones[['P1_Frecuencia', 'P1_RMS', 'P2_Frecuencia', 'P2_RMS']].values
+
+# Dividir en conjuntos de entrenamiento y prueba
+X_adelante_train, X_adelante_test, y_train, y_test = train_test_split(X_adelante, y, test_size=0.2, random_state=0, shuffle=False)
+X_atras_train, X_atras_test, _, _ = train_test_split(X_atras, y, test_size=0.2, random_state=0, shuffle=False)
+
+# Puedes imprimir las formas para confirmar
+print(X_adelante_train.shape, X_adelante_test.shape, X_atras_train.shape, X_atras_test.shape, y_train.shape, y_test.shape)
+
+class DualCNN(nn.Module):
+    def __init__(self):
+        super(DualCNN, self).__init__()
+        # Rama para mediciones delanteras
+        self.conv1_delante = nn.Conv2d(4, 16, kernel_size=3, stride=1, padding=1)
+        self.conv2_delante = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        #self.conv3_delante = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        #self.conv4_delante = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        
+        # Rama para mediciones traseras
+        self.conv1_atras = nn.Conv2d(4, 16, kernel_size=3, stride=1, padding=1)
+        self.conv2_atras = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        #self.conv3_atras = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        #self.conv4_atras = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+
+        # Definición de la CNN de Fusión
+        self.fusion_conv1 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.fusion_conv2 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+
+        # Tamaño de la salida de las capas convolucionales de fusión
+        # Ajustar según las dimensiones reales
+        salida_fusion_size = 256 * num_rows_per_case * 1
+
+        # Capa completamente conectada
+        self.fc = nn.Linear(salida_fusion_size, 4)
+
+    def forward(self, x_delante, x_atras):
+        # Rama delantera
+        x1 = F.relu(self.conv1_delante(x_delante))
+        x1 = F.relu(self.conv2_delante(x1))
+
+        # Rama trasera
+        x2 = F.relu(self.conv1_atras(x_atras))
+        x2 = F.relu(self.conv2_atras(x2))
+
+        # Concatenar manteniendo las dimensiones espaciales
+        x = torch.cat((x1, x2), dim=1)
+
+        # Pasar por la CNN de Fusión
+        x = F.relu(self.fusion_conv1(x))
+        x = F.relu(self.fusion_conv2(x))
+
+        # Aplanar antes de la capa completamente conectada
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
+
+# Convertir los datos de NumPy a tensores de PyTorch
+X_adelante_train_tensor = torch.tensor(X_adelante_train, dtype=torch.float32)
+X_atras_train_tensor = torch.tensor(X_atras_train, dtype=torch.float32)
+y_train_tensor = torch.tensor(y_train, dtype=torch.float32)
+
+X_adelante_test_tensor = torch.tensor(X_adelante_test, dtype=torch.float32)
+X_atras_test_tensor = torch.tensor(X_atras_test, dtype=torch.float32)
+y_test_tensor = torch.tensor(y_test, dtype=torch.float32)
+
+# Instanciar el modelo
+model = DualCNN()
+
+# Definir la función de pérdida y el optimizador
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.00001)
+
+# Entrenamiento
+num_epochs = 100  # Ajusta según sea necesario
+
+for epoch in range(num_epochs):
+    model.train()
+    for i in range(X_adelante_train_tensor.size(0)):
+        # Obtener lotes de datos
+        data_delante = X_adelante_train_tensor[i].unsqueeze(0)
+        data_atras = X_atras_train_tensor[i].unsqueeze(0)
+        labels = y_train_tensor[i].unsqueeze(0)
+
+        # Paso hacia adelante
+        outputs = model(data_delante, data_atras)
+        loss = criterion(outputs, labels)
+
+        # Paso hacia atrás y optimizar
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}')
+
+model.eval()
+predictions = []
+with torch.no_grad():
+    for i in range(len(X_adelante_test)):
+        data_delante = torch.tensor(X_adelante_test[i]).unsqueeze(0).float()
+        data_atras = torch.tensor(X_atras_test[i]).unsqueeze(0).float()
+        output = model(data_delante, data_atras)
+        predictions.append(output.numpy())
+
+predictions = np.vstack(predictions)
+
+# Suponiendo que 'predictions' es un array con tus predicciones y 'y_test' son los valores reales
+mse = mean_squared_error(y_test, predictions)
+print("Mean Squared Error:", mse)
+features = ['P1_Frecuencia', 'P1_RMS', 'P2_Frecuencia', 'P2_RMS']
+mse_per_feature = {}
+
+for i, feature in enumerate(features):
+    mse = mean_squared_error(y_test[:, i], predictions[:, i])
+    mse_per_feature[feature] = mse
+
+# Mostrar el MSE por cada característica
+for feature, mse in mse_per_feature.items():
+    print(f"MSE para {feature}: {mse}")
+    
+# Graficar el ground truth vs predicciones
+fig, axes = plt.subplots(2, 2, figsize=(12, 8))
+features = ['P1_Frecuencia', 'P1_RMS', 'P2_Frecuencia', 'P2_RMS']
+
+for i, feature in enumerate(features):
+    ax = axes[i//2, i%2]
+    # Ground truth en puntos
+    ax.scatter(range(len(y_test[:, i])), y_test[:, i], alpha=0.7, label='Ground Truth')
+    # Predicciones en puntos
+    ax.scatter(range(len(predictions[:, i])), predictions[:, i], alpha=0.7, label='Predictions')
+    ax.set_xlabel('Samples')
+    ax.set_ylabel(feature)
+    ax.set_title(f'Comparison for {feature}')
+    ax.legend()
+
+plt.tight_layout()
+plt.show()
